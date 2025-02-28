f332860a9858b791317f00d7c92ab098734be28e
esha pentakota <eshapent@eshas-MacBook-Pro.local>
2025-02-28 12:44:29 -0500

    calling the function

--- Backend/scripts_for_frontend/main_script_for_backend_json_to_csv.py
+++ Backend/scripts_for_frontend/main_script_for_backend_json_to_csv.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request, send_file
 from graph_maker_v2 import build_prereq_graph, build_selected_courses_graph, visualize_selected_courses_graph
-from csv_to_curricular import return_all_courses_json, index_courses, info_courses, prereq_courses, prefix_courses, save_to_csv  # Import necessary functions
+from csv_to_curricular import generate_curricular_csv 
 import json
 from flask_cors import CORS
 import os
@@ -55,52 +55,10 @@ def generate_csv():
         return jsonify({"error": "No selected courses provided"}), 400
 
     try:
-        # generate the csv
-        output_folder = "Backend/frontend_files/"
-        output_file = os.path.join(output_folder, "csv_for_display.csv")
+        # generate the CSV using function created
+        output_file = generate_curricular_csv(selected_courses)
 
-        # select courses
-        courses = return_all_courses_json("Backend/frontend_files/courses_picked.json")
-
-    
-        all_courses = index_courses(courses)
-        info_courses(all_courses)
-        prereq_courses(all_courses)
-        prefix_courses(all_courses)
-
-        # course data for csv
-        courses_info = [
-            {
-                "Course ID": index,
-                "Course Name": course[0],
-                "Prefix": course[1],
-                "Number": course[2],
-                "Prerequisites": course[3],
-                "Corequisites": course[4],
-                "Strict-Corequisites": course[5],
-                "Credit Hours": course[6],
-                "Institution": "GT",
-                "Canonical Name": course[8]
-            }
-            for index, course in all_courses.items()
-        ]
-
-        # metadata for csv
-        metadata = [
-            {"Field": "Curriculum", "Value": "Major"},
-            {"Field": "Institution", "Value": "Georgia Institute of Technology"},
-            {"Field": "Degree Type", "Value": "BS"},
-            {"Field": "System Type", "Value": "Semester"},
-            {"Field": "CIP", "Value": ""},
-            {"Field": "Courses", "Value": ""}
-        ]
-
-        # save CSV
-        df2 = pd.DataFrame(metadata)
-        df = pd.DataFrame(courses_info)
-        save_to_csv(df, df2, output_file)
-
-        # Send the generated CSV file
+        # return the CSV file
         return send_file(output_file, mimetype="text/csv", as_attachment=True, download_name="curriculum.csv")
 
     except Exception as e:
