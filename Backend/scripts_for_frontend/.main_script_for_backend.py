84d75db43781bad78dbb5817eaeb3a6c19eb818d
Hossain, Shahbin <shossain43@gatech.edu>
2025-03-26 11:55:18 -0400

    Tweaked Majors Folder (#40)
    * Tweaked college_data.json and majors folder
    
    * reformatted names in majors folder

--- Backend/scripts_for_frontend/.main_script_for_backend.py
+++ Backend/scripts_for_frontend/.main_script_for_backend.py
@@ -108,8 +108,9 @@ def get_program_table():
             #print(f"Checking file: {file} => Filename: {normalized_filename}")
 
             if all(word in normalized_filename for word in selected_program):
-                matching_file = file
-                break  # Stop at the first match
+                if file.split("-")[0] in selected_program[0]:
+                    matching_file = file
+                    break  # Stop at the first match
 
         if not matching_file:
             print("No matching file found.")
